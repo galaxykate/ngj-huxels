beb86df9ea4c3956c63e81808a2d9d24555cc205
naomi.lea <naomi.lea@lego.com>
2024-04-21 14:38:13 +0200

    stick figure sketch

---
+++ huxel-start.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ **/
+
+MODES.start = {
+	start({}) {
+		console.log("START TEST")
+	},
+
+	stop({p, tracker, huxels, time, particles, debugOptions}) {
+		console.log("STOP TEST")
+	},
+
+	update({p, tracker, huxels, time, particles, debugOptions}) {
+		if (Math.random() > .9) {
+			let x = randInt(0, 10)
+			let y = randInt(0, 10)
+			huxels.push(new Huxel(x, y))
+		}
+
+	},
+
+	drawBackground({p, tracker, huxels, time, particles, debugOptions}) {
+		// p.background(50, 100, 90)
+	},
+
+	draw({p, tracker, huxels, time, particles, debugOptions}) {
+		p.fill(300, 80, 50)
+		// p.circle(0, 0, 500)
+		p.text("TEST APP", 100, 100)
+
+
+	},
+	
+}
