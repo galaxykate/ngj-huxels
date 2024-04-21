28901025239c385d6407343949e6821615ea64a0
w-b-horslund <w.b.horslund@gmail.com>
2024-04-21 12:59:41 +0200

    Sound assets added to script

--- assets.js
+++ assets.js
@@ -21,9 +21,33 @@ let IMAGE_ASSET_FILES = {
 }
 
 let SOUND_ASSET_FILES = {
+	// Tetris sounds
 	"tetris1": "MUS_Tetris_Level1.wav",
+	"tetris2": "MUS_Tetris_Level2.wav",
+	"tetris3": "MUS_Tetris_Level3.wav",
+	"tetris4": "MUS_Tetris_Level4.wav",
+	"tetris5": "MUS_Tetris_Level5.wav",
+	"tetris6": "MUS_Tetris_Level6.wav",
+	"tetris7": "MUS_Tetris_Level7.wav",
+	"tetris8": "MUS_Tetris_Level8.wav",
+	"tetris9": "MUS_Tetris_Level9.wav",
+	"tetrisLineCleared0": "S_Tetris_LineCleared0.wav",
+	"tetrisLineCleared1": "S_Tetris_LineCleared1.wav",
+	"tetrisLineCleared2": "S_Tetris_LineCleared2.wav",
+	"tetrisBlockCreated0": "S_Tetris_BlockCreated0.wav",
+	"tetrisBlockCreated1": "S_Tetris_BlockCreated1.wav",
+	"tetrisBlockCreated2": "S_Tetris_BlockCreated2.wav",
+	"tetrisBlockPlaced0": "S_Tetris_BlockPlaced0.wav",
+	"tetrisBlockPlaced1": "S_Tetris_BlockPlaced1.wav",
+	"tetrisBlockPlaced2": "S_Tetris_BlockPlaced2.wav",
+
+	// Stick and Bird sounds
+	"birdLaser0": "S_LaserBird_Laser0.wav",
+	"birdLaser1": "S_LaserBird_Laser1.wav",
+	"birdLaser2": "S_LaserBird_Laser2.wav",
     "stick_dance": "S_Stick_DanceLoop.wav",
-    //
+
+	// Bee sounds
     "beeAmbience": "S_Bee_Ambience.wav",
 	"beeLoop": "S_Bee_BeeLoop.wav",
 	"beePollinateLoop": "S_Bee_PolinateLoop.wav",
@@ -31,6 +55,22 @@ let SOUND_ASSET_FILES = {
 	"beeDropOff0": "S_Bee_DropOff0.wav",
 	"beeDropOff1": "S_Bee_DropOff1.wav",
 	"beeDropOff2": "S_Bee_DropOff2.wav",
+
+	// Monster Sounds
+	"monsterAmbience": "S_Monster_Ambience.wav",
+	"monsterGrab0": "S_Monster_Grab0.wav",
+	"monsterGrab1": "S_Monster_Grab1.wav",
+	"monsterGrab2": "S_Monster_Grab2.wav",
+	"monsterGrab3": "S_Monster_Grab3.wav",
+	"monsterGrab4": "S_Monster_Grab4.wav",
+	"monsterGulp0": "S_Monster_Gulp0.wav",
+	"monsterGulp1": "S_Monster_Gulp1.wav",
+	"monsterGulp2": "S_Monster_Gulp2.wav",
+	"monsterGulp3": "S_Monster_Gulp3.wav",
+	"monsterGulp4": "S_Monster_Gulp4.wav",
+	"monsterGulp5": "S_Monster_Gulp5.wav",
+
+
 }
 
 let IMAGE = {}
