5340bc5cc98e9cb23bfd79c15012a3cc813a5883
kasperab <kasperab@yahoo.se>
2024-04-25 16:20:01 +0200

    Added sound and score to clean mode

--- huxel-clean.js
+++ huxel-clean.js
@@ -1,17 +1,20 @@
 MODES.clean = {
 	dirt: [],
 	stars: [],
+	countdown: 0,
 
 	start({p, tracker, huxels, time, particles, debugOptions}) {
 		tracker.scale = 6
 		this.dirt = []
 		this.stars = []
-		for (let i = 0; i < 256; i++) {
-			this.dirt.push(new Vector2D(Math.random() * 1664 + 128, Math.random() * 854 + 128))
+		for (let index = 0; index < 256; index++) {
+			this.dirt.push(new Vector2D(Math.random() * (p.width - 256) + 128, Math.random() * (p.height - 256) + 128))
 		}
+		this.countdown = 30000
 	},
 
 	stop({p, tracker, huxels, time, particles, debugOptions}) {
+		SOUND.windowWipe.stop()
 	},
 
 	update({p, tracker, huxels, time, particles, debugOptions}) {
@@ -20,22 +23,29 @@ MODES.clean = {
 				let position = hand.center
 				for (let index = 0; index < this.dirt.length; index++) {
 					if (Vector2D.distance(position, this.dirt[index]) < 64) {
-						if (Math.random() < 0.3) {
+						if (Math.random() < 0.1) {
 							this.stars.push(this.dirt[index])
+							app.score.value += 50
+						} else {
+							app.score.value += 10
 						}
 						this.dirt.splice(index, 1)
 						index--
+						if (!SOUND.windowWipe.isPlaying()) {
+							SOUND.windowWipe.play()
+						}
 					}
 				}
 			}
 		})
-		if (this.dirt.length <= 0) {
+		this.countdown -= time.dt
+		if (this.countdown <= 0) {
 			debugOptions.mode = "tetris"
 		}
 	},
 
 	drawBackground({p, tracker, huxels, time, particles, debugOptions}) {
-		p.image(IMAGE.cleanMirror, 0, 0, 1920, 1080)
+		p.image(IMAGE.cleanMirror, 0, 0, p.width, p.height)
 	},
 
 	draw({p, tracker, huxels, time, particles, debugOptions}) {
